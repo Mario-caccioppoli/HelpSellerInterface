ab6204b49c6a3e60dae4325a2f69d30b7b19b23b
Frank94 <f.ciampa2@studenti.unisa.it>
2021-12-28 17:16:56 +0100

    Aggiunte componenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/navbar/footer/footer.component';
+import { LoginComponent } from './components/navbar/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    FooterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,

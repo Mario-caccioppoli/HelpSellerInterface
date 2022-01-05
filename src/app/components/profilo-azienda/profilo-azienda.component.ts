42efecf4aa1ffddb045ca9e153ae2d65729bd7c4
Frank94 <f.ciampa2@studenti.unisa.it>
2022-01-05 17:13:04 +0100

    rinomino componente in profilo-azienda

--- src/app/components/profilo-azienda/profilo-azienda.component.ts
+++ src/app/components/profilo-azienda/profilo-azienda.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pagina-azienda',
+  selector: 'app-profilo-azienda',
   templateUrl: './profilo-azienda.component.html',
   styleUrls: ['./profilo-azienda.component.css']
 })

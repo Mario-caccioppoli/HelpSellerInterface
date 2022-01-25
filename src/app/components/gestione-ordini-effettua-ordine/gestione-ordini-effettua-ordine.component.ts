ba04b4e153a5a0cc28919b619aead96a01d7993a
Frank94 <f.ciampa2@studenti.unisa.it>
2022-01-25 12:02:17 +0100

    gestione ordini implementata una prima logica per effettuare un ordine

--- src/app/components/gestione-ordini-effettua-ordine/gestione-ordini-effettua-ordine.component.ts
+++ src/app/components/gestione-ordini-effettua-ordine/gestione-ordini-effettua-ordine.component.ts
@@ -5,6 +5,10 @@ import { ProdottoService } from 'src/app/services/prodotto/prodotto.service';
 import { LogService } from 'src/app/services/log.service';
 import { ActivatedRoute } from '@angular/router';
 import { Ordine } from 'src/app/models/Ordine';
+import { Utente } from 'src/app/models/Utente';
+import { OrdineProdotto } from 'src/app/models/OrdineProdotto';
+import { OrdineProdottoService } from 'src/app/services/ordine-prodotto/ordine-prodotto.service';
+import { OrdineService } from 'src/app/services/ordine/ordine.service';
 @Component({
   selector: 'app-gestione-ordini-effettua-ordine',
   templateUrl: './gestione-ordini-effettua-ordine.component.html',
@@ -12,14 +16,22 @@ import { Ordine } from 'src/app/models/Ordine';
 })
 export class GestioneOrdiniEffettuaOrdineComponent implements OnInit {
 
-  constructor(private prodottoService : ProdottoService, private route: ActivatedRoute, private log: LogService) { }
-
+  constructor(private prodottoService : ProdottoService, private route: ActivatedRoute, private log: LogService,private ordineProdottoService:OrdineProdottoService, private ordineService:OrdineService) { }
+  //FORSE NON SERVONO
   idAzienda: number;
-  prodotti : Prodotto[];
-  ricercaProdottoByNome: any;
-  ordine: Ordine;
   product: Prodotto;
-  myStorage = window.localStorage;
+  ricercaProdottoByNome: any;
+
+  //PER COSTRUIRE ORDINE
+  ordine: Ordine;
+  ordineProdottoList: OrdineProdotto[]=[];
+  prodottiInOrdine: Prodotto[];
+  number:number[]=[]
+  
+  //PER FUNZIONALITA
+  cercaNomeProdotto:string;  
+  currentUser:Utente=JSON.parse(localStorage.getItem('currentUser'));
+  prodotti : Prodotto[]; //getall prodotti
 
   ngOnInit(): void {
    /* this.prendiIdAziendaDalRouter()
@@ -47,9 +59,6 @@ export class GestioneOrdiniEffettuaOrdineComponent implements OnInit {
       nascondi[k].style.display = 'none';
     }
 
-    var max = <HTMLInputElement>document.getElementById("qtaprod");;
-    max.setAttribute("max", '10000');
-    max.setAttribute("value", '0');
   }
 
   exitCart() {  //annulla la procedura di acquisto
@@ -101,11 +110,6 @@ export class GestioneOrdiniEffettuaOrdineComponent implements OnInit {
 //////////////////////////////////////                         //////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  prendiIdAziendaDalRouter(){
-    this.route.paramMap.subscribe(params=>
-      this.idAzienda= +params.get('id'))
-  }
-
   getAllProdotto(){
     this.prodottoService.getAllProdotto().subscribe(
       (resp)=>{
@@ -116,106 +120,46 @@ export class GestioneOrdiniEffettuaOrdineComponent implements OnInit {
         this.log.Error(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",[error]);
       }
     )
-  }
-
-  getProdottiByIdAzienda(){
-    this.prodottoService.getProdottoByIdAzienda(this.idAzienda).subscribe(
-      (resp) => {
-        this.log.Debug(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",[resp]);
-        this.prodotti = resp as Prodotto[];
-      },
-      (error) => {
-        this.log.Error(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",[error]);
-      }
-    )
-  }
-
-  ricercaConSidebar(form){
-    if(form.searchbar==''){
-      this.getProdottiByIdAzienda();
-    }
-    else{
-    console.log(this.idAzienda)
-    console.log(form.searchbar)
-    this.prodottoService.findProdottiByNomeInAzienda(form.searchbar,this.idAzienda).subscribe(
-      (resp)=>{
-        this.log.Debug(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",resp);
-        this.prodotti = resp as Prodotto[];
-        console.log(resp)
-      },
-      (error)=>{
-        this.log.Error(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",[error]);
-      }
-    )
-    }
-  }
-
-  initializeCart(){
-
-    this.prodottoService.getAllProdotto().subscribe(
-      (resp)=>{
-        this.log.Debug(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",[resp]);
-
-       // this.aggiungiAlCarrello(this.prodotti);
-
-        this.prodotti = resp as Prodotto[];
-      },
-      (error)=>{
-        this.log.Error(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",[error]);
-      }
-    )
-  }
-
-  
-
+  }  
 
 
 // Inizio procedura Carrello
 
   avviaCarrello(selezione) {
 
-    this.product = {
-      id: selezione.id,
-      nomeProdotto: null,
-      prezzo: null,
-      descrizione: null,
-      quantita: selezione.quantita,
-      immagine: null,
-      quantitaMinima: null,
-      peso: null,
-      volume: null,
-      idAzienda: null,
-      recensioni: null,
-      sconti: null
-    }
-
-    let local_storage;
-    let itemsInCart = []
+    //var container=document.getElementById("prodottiContainer"+' '+selezione.id);
+    var check=document.getElementById("checkbox"+' '+selezione.id);
+    var qtaprod=document.getElementById("qtaprod"+' '+selezione.quantita);
     
-    if(localStorage.getItem('carrello')  == (null || undefined)){
-      local_storage =[];
-      console.log("LocalStorage vuoto",JSON.parse(localStorage.getItem('carello')));
-      itemsInCart.push(this.prodotti);
 
-      localStorage.setItem('cart', JSON.stringify(itemsInCart));
-      console.log('Inserito: ', itemsInCart);
-    }
-    else
-    {
-      local_storage = JSON.parse(localStorage.getItem('carrello'));
-      console.log("LocalStorage non vuoto",JSON.parse(localStorage.getItem('cart')));
-      for(var i in local_storage)
-      {
-        console.log(local_storage[i].prodotti.prodotto.id);
-        if(this.product.id == local_storage[i].prodotti.prodotto.id)
-        {
-          local_storage[i].quantity += 1;
-          console.log("Quantita prodotto "+i+" : "+ local_storage[i].quantita);
-          console.log('Gia inserito, id ', i); 
-          this.product=null;
-          break;  
-        }
-    }
+
+    // let local_storage;
+    // let itemsInCart = []
+    
+    // if(localStorage.getItem('carrello')  == (null || undefined)){
+    //   local_storage =[];
+    //   console.log("LocalStorage vuoto",JSON.parse(localStorage.getItem('carello')));
+    //   itemsInCart.push(this.prodotti);
+
+    //   localStorage.setItem('cart', JSON.stringify(itemsInCart));
+    //   console.log('Inserito: ', itemsInCart);
+    // }
+    // else
+    // {
+    //   local_storage = JSON.parse(localStorage.getItem('carrello'));
+    //   console.log("LocalStorage non vuoto",JSON.parse(localStorage.getItem('cart')));
+    //   for(var i in local_storage)
+    //   {
+    //     console.log(local_storage[i].prodotti.prodotto.id);
+    //     if(this.product.id == local_storage[i].prodotti.prodotto.id)
+    //     {
+    //       local_storage[i].quantity += 1;
+    //       console.log("Quantita prodotto "+i+" : "+ local_storage[i].quantita);
+    //       console.log('Gia inserito, id ', i); 
+    //       this.product=null;
+    //       break;  
+    //    }
+    //}
     
     
     /*if(this.product){
@@ -226,12 +170,75 @@ export class GestioneOrdiniEffettuaOrdineComponent implements OnInit {
     })
     localStorage.setItem('cart', JSON.stringify(itemsInCart));*/
 
-    } 
+   // } 
   }
 
+  cercaChange(){
+    this.prodottoService.findAllProdottiByNome(this.cercaNomeProdotto).subscribe(
+      (resp)=>{
+        this.log.Debug(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",[resp]);
+        this.prodotti=resp;
+      },
+      (error)=>{
+        this.log.Error(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",[error]);
+      }
+    )
+  }
 
+  selezionaProdotti(event,prodotto:Prodotto){
+    if(event!=undefined && prodotto.id!=undefined && this.ordineProdottoList!=undefined){
+      let quantita=event.target.value;
+      var ordineProdotto=this.ordineProdottoList.find(p=>p.prodotto.id==prodotto.id)
+      if(ordineProdotto!=undefined){
+        ordineProdotto.quantitaOrdine=quantita;
+      }
+      else{
+        var ord:OrdineProdotto={
+          idOrdine:null,
+          prezzoUnitario:prodotto.prezzo,
+          prodotto:prodotto,
+          quantitaOrdine:quantita
+        }
+       this.ordineProdottoList.push(ord);
+      }
+    }
+  }
+    
+    eseguiOrdine() {
+      //to do add form comment
+      var PrezzoTotale=0;
+      var dataOggi=new Date();
+      var dataConsegna=new Date();
+      dataConsegna.setMonth(dataConsegna.getMonth()+2);
+      this.ordineProdottoList.forEach(ordineProdotto => {
+        PrezzoTotale+=ordineProdotto.prezzoUnitario*ordineProdotto.quantitaOrdine;
+      });
+      if(this.ordine==undefined){
+        this.ordine={
+          id:null,
+          commento:"",
+          dataConsegna:dataConsegna,
+          dataOrdinazione:dataOggi,
+          documento:null,
+          idDistributore:this.currentUser.id,
+          ordineProdotti:this.ordineProdottoList,
+          prezzoTotale:PrezzoTotale,
+          stato:"created",
+        }
+      }
+      this.ordineService.insertOrdine(this.ordine).subscribe(
+        (resp)=>{
+          this.log.Debug(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",[resp]);
+          window.alert("ORDINE EFFETTUATO")
+        },
+        (error)=>{
+          this.log.Error(GestioneOrdiniEffettuaOrdineComponent.name,"chiamata a back-end",[error]);
+        }
+    ) 
 
+  
 
+    }
 
   
 }

5f8f1207198f118bca781f636ec2fe1c5a5df095
Antonio Abate Chechile <antonioab17@yahoo.it>
2022-01-28 23:34:53 +0100

    gestione ordini

--- src/app/components/gestione-ordini-effettuati/gestione-ordini-effettuati.component.ts
+++ src/app/components/gestione-ordini-effettuati/gestione-ordini-effettuati.component.ts
@@ -2,7 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Local } from 'protractor/built/driverProviders';
 import { Distributore } from 'src/app/models/Distributore';
 import { Ordine } from 'src/app/models/Ordine';
+import { OrdineProdotto } from 'src/app/models/OrdineProdotto';
 import { LogService } from 'src/app/services/log.service';
+import { OrdineProdottoService } from 'src/app/services/ordine-prodotto/ordine-prodotto.service';
 import { OrdineService } from 'src/app/services/ordine/ordine.service';
 
 @Component({
@@ -14,21 +16,59 @@ export class GestioneOrdiniEffettuatiComponent implements OnInit {
 
   singleOrdine: Ordine[];
   distributore: Distributore;
+  ordineProdotto: OrdineProdotto[];
+  ordine: Ordine;
+  myStorage = window.localStorage;
 
-  constructor(private prod: OrdineService, private log: LogService) { }
+  prezzoTotale: number = 0;
+
+  constructor(private os: OrdineService, private ops: OrdineProdottoService, private log: LogService) { }
 
   ngOnInit(): void {
-    //this.distributore =  JSON.parse(localStorage.getItem("IDutente")) as Distributore;
-    if(this.distributore != undefined)
+    this.myStorage.getItem('currentUser');
+ 
+    if((this.myStorage.getItem('currentUser') != undefined) && this.checktypeD())
     {
+      this.distributore = JSON.parse(this.myStorage.getItem('currentUser')) as Distributore;
       this.listaOrdini();
     }
   }
 
+  prezzoOrdine(ord) {
+    this.ops.findDettagliOrdine(ord).subscribe(
+      (success) => {
+        this.log.Debug(GestioneOrdiniEffettuatiComponent.name, "ok", [success]);
+
+        this.ordineProdotto = success as OrdineProdotto[];
+        
+        var quantitaSingola; var prezzoSingolo; var prezzoQuantita; var totalPrice = 0;
+        
+        for (var i=0; i<=this.ordineProdotto.length; i++) {
+          quantitaSingola = this.ordineProdotto[i].quantitaOrdine;
+          prezzoSingolo = this.ordineProdotto[i].prezzoUnitario;
+          prezzoQuantita = quantitaSingola * prezzoSingolo;
+          totalPrice += prezzoQuantita;
+        }
+
+        this.prezzoTotale = totalPrice;
+
+      },
+
+      (error) => {
+        this.log.Error(GestioneOrdiniEffettuatiComponent.name, "errore", [error]);
+      }
+    )
+}
+
   listaOrdini() {
-      this.prod.getAllOrdinebyDistributore(this.distributore.id).subscribe(
+      this.os.getAllOrdinebyDistributore(this.distributore.id).subscribe(
         (success) => {
           this.log.Debug(GestioneOrdiniEffettuatiComponent.name, "ok", [success]);
+
+          for (var k=0; k<=this.ordineProdotto.length; k++) {
+            this.prezzoOrdine(this.ordine.id)
+          }
+
           this.singleOrdine = success as Ordine[];
         },
 
@@ -36,6 +76,17 @@ export class GestioneOrdiniEffettuatiComponent implements OnInit {
           this.log.Error(GestioneOrdiniEffettuatiComponent.name, "errore", [error]);
         }
       )
-  } //end func
+  }
+
+  checktypeD() {
+    const account = this.myStorage.getItem('currentUser');
+    const obj = JSON.parse(account);
+    const tipo = obj.tipo;
+    if (tipo == "Distributore") {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
